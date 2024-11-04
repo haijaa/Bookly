f77912af38d3903483f9d3898fc6183a37dcf721
Maja Lennevi <majlen30@gmail.com>
2024-11-04 16:51:32 +0100

    Created nav bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import { Accordion } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function MyComponent() {
   return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Antons</Accordion.Header>
-        <Accordion.Body>Antons låda</Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Majas</Accordion.Header>
-        <Accordion.Body>Majas låda</Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Frida</Accordion.Header>
-        <Accordion.Body>Fridas låda</Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+    <>
+      <NavBar />
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Antons</Accordion.Header>
+          <Accordion.Body>Antons låda</Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Majas</Accordion.Header>
+          <Accordion.Body>Majas låda</Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Frida</Accordion.Header>
+          <Accordion.Body>Fridas låda</Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
   );
 }
 
