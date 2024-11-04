4fa32328d9b83ca5165a6e29a8721cf8a2ac8f2b
haijaa <136151213+haijaa@users.noreply.github.com>
2024-11-04 15:52:52 +0100

    Added loginform. (#2)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,7 @@
-import { Accordion } from "react-bootstrap";
+import Login from "./components/LoginForm";
 
 function MyComponent() {
-  return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Antons</Accordion.Header>
-        <Accordion.Body>Antons låda</Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Majas</Accordion.Header>
-        <Accordion.Body>Majas låda</Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Frida</Accordion.Header>
-        <Accordion.Body>Fridas låda</Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-  );
+  return <Login />;
 }
 
 export default MyComponent;
