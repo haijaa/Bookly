ff0d14a98f7e569f24b64ed160668144a902b748
Maja Lennevi <majlen30@gmail.com>
2024-11-05 17:01:50 +0100

    Created form for user

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import Login from "./components/LoginForm";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import UserForm from "./components/UserForm";
 
 function MyComponent() {
   return (
     <>
       <NavBar />
       <main className="min-vh-100">
-        <Login />;
+        <Login />
+        <UserForm />
       </main>
       <Footer />
     </>
