40b1362819d9906224831224e1d89f9ec6980ed8
haijaa <antkarl11@gmail.com>
2024-11-05 16:55:39 +0100

    Added endpoints for books and books by id. Made a component to handle fetch for books.

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { Client } = require("pg");
 
-const app = express(),
-  port = process.env.PORT || 3000;
+dotenv.config();
 
-app.get("/", (_request, response) => {
-  response.send({
-    hello: "World",
-    Frida: "Wikman",
-    Anton: "Karlsson",
-    Maja: "Lennevi",
-  });
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/books", async (_request, response) => {
+  const { rows } = await client.query("SELECT * from books;");
+  response.send(rows);
+});
+
+// Rewrite query to get genres too. Also applies to /API/BOOKS
+
+app.get("/api/books/:id", async (request, response) => {
+  const { id } = request.params;
+  const { rows } = await client.query("SELECT * FROM books WHERE bookId = $1", [
+    id,
+  ]);
+  response.send(rows);
 });
 
 app.listen(port, () => {
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import Login from "./components/LoginForm";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import AllBooks from "./components/AllBooks";
 
 function MyComponent() {
   return (
     <>
       <NavBar />
       <main className="min-vh-100">
-        <Login />;
+        <Login />
       </main>
       <Footer />
     </>
