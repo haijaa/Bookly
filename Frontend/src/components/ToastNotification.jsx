1fff14bd8b023e0c5f2c63db8e72ff38f004fbcb
Maja Lennevi <majlen30@gmail.com>
2024-11-08 15:52:15 +0100

    Created notification toast and modal for validation when updating data

---
+++ Frontend/src/components/ToastNotification.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
+
+ToastNotification.propTypes = {
+  show: PropTypes.bool,
+  title: PropTypes.string,
+  message: PropTypes.string,
+};
+
+export default function ToastNotification({ title, message, show }) {
+  return (
+    <div
+      aria-live="polite"
+      aria-atomic="true"
+      className="bg-dark position-relative"
+    >
+      <ToastContainer
+        position="top-center"
+        className="p-3"
+        style={{ zIndex: 1, position: "fixed" }}
+      >
+        <Toast bg="light" show={show} autohide>
+          <Toast.Header>
+            <img src="holder.js/20x20?text=%20" className="rounded me-2" />
+            <strong className="me-auto">{title}</strong>
+          </Toast.Header>
+          <Toast.Body>{message}</Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </div>
+  );
+}
