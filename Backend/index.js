e77f9514286e30699d65672b988f2ada69ec9b50
FridaWikman <frida.wikman@hotmail.com>
2024-11-08 14:51:03 +0100

    Post review

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const { Client } = require("pg");
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const { Client } = require('pg')
 
-dotenv.config();
+dotenv.config()
 
 const client = new Client({
   connectionString: process.env.PGURI,
-});
+})
 
-client.connect();
+client.connect()
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get("/api/books", async (_request, response) => {
-  const { rows } = await client.query("SELECT * from books;");
-  response.send(rows);
-});
+app.get('/api/books', async (_request, response) => {
+  const { rows } = await client.query('SELECT * from books;')
+  response.send(rows)
+})
 
-app.get("/api/books/:id", async (request, response) => {
-  const { id } = request.params;
+app.get('/api/books/:id', async (request, response) => {
+  const { id } = request.params
   const { rows } = await client.query(
     `
     SELECT
@@ -64,63 +64,63 @@ app.get("/api/books/:id", async (request, response) => {
     GROUP BY
         books.bookId;
     `,
-    [id],
-  );
-  response.send(rows);
-});
+    [id]
+  )
+  response.send(rows)
+})
 
-app.post("/api/reviews", async (request, response) => {
-  const { reviewContent, reviewUserId, reviewBookId } = request.body;
+app.post('/api/reviews', async (request, response) => {
+  const { reviewContent, reviewUserId, reviewBookId } = request.body
   try {
     const { rows } = await client.query(
-      "INSERT INTO reviews (reviewContent, revuewUserId , reviewBookId) VALUES ($1, $2, $3)",
-      [reviewContent, reviewUserId, reviewBookId],
-    );
-    response.status(200).json("Added:", rows[0]);
+      'INSERT INTO reviews (reviewContent, reviewUserId , reviewBookId) VALUES ($1, $2, $3) RETURNING *',
+      [reviewContent, reviewUserId, reviewBookId]
+    )
+    response.status(200).json({ message: 'Added:', data: rows[0] })
   } catch (error) {
-    console.log("Error: ", error);
-    response.status(500).send("Error on serverside");
+    console.log('Error: ', error)
+    response.status(500).send('Error on serverside')
   }
-});
+})
 
-app.delete("/api/reviews/:bookId", async (request, response) => {
-  const { reviewId } = request.body;
+app.delete('/api/reviews/:bookId', async (request, response) => {
+  const { reviewId } = request.body
   try {
     const { rows } = await client.query(
-      "DELETE FROM reviews WHERE reviewId = $1",
-      [reviewId],
-    );
-    response.status(200).json("Review deleted");
+      'DELETE FROM reviews WHERE reviewId = $1',
+      [reviewId]
+    )
+    response.status(200).json('Review deleted')
   } catch (error) {
-    response.status(500).json("Error on serverside");
+    response.status(500).json('Error on serverside')
   }
-});
+})
 
-app.get("/api/users", async (request, response) => {
-  const { rows } = await client.query("SELECT * FROM users;");
-  response.send(rows);
-});
+app.get('/api/users', async (request, response) => {
+  const { rows } = await client.query('SELECT * FROM users;')
+  response.send(rows)
+})
 
-app.post("/api/users", async (request, response) => {
-  const { userFullName, userEmail, userUserName, userPassword } = request.body;
+app.post('/api/users', async (request, response) => {
+  const { userFullName, userEmail, userUserName, userPassword } = request.body
   try {
     const { rows } = await client.query(
-      "INSERT INTO users (userFullname, userEmail, userUserName, userPassword) VALUES ($1, $2, $3, $4) RETURNING *;",
-      [userFullName, userEmail, userUserName, userPassword],
-    );
+      'INSERT INTO users (userFullname, userEmail, userUserName, userPassword) VALUES ($1, $2, $3, $4) RETURNING *;',
+      [userFullName, userEmail, userUserName, userPassword]
+    )
     response
       .status(201)
-      .json({ message: "User created successfully", user: rows[0] });
+      .json({ message: 'User created successfully', user: rows[0] })
   } catch (error) {
-    console.error("Error create user", error);
+    console.error('Error create user', error)
     response.status(500).send({
-      message: "Error create user",
+      message: 'Error create user',
       error: error.message,
-    });
+    })
   }
-});
+})
 
-app.put("/api/users", async (request, response) => {
+app.put('/api/users', async (request, response) => {
   const {
     userId,
     userFullName,
@@ -128,7 +128,7 @@ app.put("/api/users", async (request, response) => {
     userUserName,
     userPassword,
     userProfilePicture,
-  } = request.body;
+  } = request.body
   try {
     const { rows } = await client.query(
       `UPDATE users SET userFullName = $1,
@@ -143,39 +143,39 @@ app.put("/api/users", async (request, response) => {
         userPassword,
         userProfilePicture,
         userId,
-      ],
-    );
+      ]
+    )
     response
       .status(201)
-      .json({ message: "User updated successfully", user: rows[0] });
+      .json({ message: 'User updated successfully', user: rows[0] })
   } catch (error) {
-    console.error("Error update user", error);
+    console.error('Error update user', error)
     response.status(500).send({
-      message: "Error update user",
+      message: 'Error update user',
       error: error.message,
-    });
+    })
   }
-});
+})
 
-app.delete("/api/users", async (request, response) => {
-  const { userId } = request.body;
+app.delete('/api/users', async (request, response) => {
+  const { userId } = request.body
   try {
     const { rows } = await client.query(
-      "DELETE FROM users WHERE userId = $1 RETURNING *;",
-      [userId],
-    );
+      'DELETE FROM users WHERE userId = $1 RETURNING *;',
+      [userId]
+    )
     response
       .status(201)
-      .json({ message: "User deleted successfully", user: rows[0] });
+      .json({ message: 'User deleted successfully', user: rows[0] })
   } catch (error) {
-    console.error("Error delete user", error);
+    console.error('Error delete user', error)
     response.status(500).send({
-      message: "Error delete user",
+      message: 'Error delete user',
       error: error.message,
-    });
+    })
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Redo på http://localhost:${port}/`);
-});
+  console.log(`Redo på http://localhost:${port}/`)
+})
