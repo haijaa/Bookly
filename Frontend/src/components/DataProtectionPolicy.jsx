f51c96cd6423743fa145cb7d41c214af77e2d831
FridaWikman <frida.wikman@hotmail.com>
2024-11-12 14:19:10 +0100

    Added first two section in DataProtectionPolicy-modal

--- Frontend/src/components/DataProtectionPolicy.jsx
+++ Frontend/src/components/DataProtectionPolicy.jsx
@@ -1,11 +1,11 @@
-import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import PropTypes from 'prop-types'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 DataProtectionPolicy.propTypes = {
   showModal: PropTypes.bool,
   onClose: PropTypes.func.isRequired,
-};
+}
 
 export default function DataProtectionPolicy({ showModal, onClose }) {
   return (
@@ -21,13 +21,35 @@ export default function DataProtectionPolicy({ showModal, onClose }) {
       </Modal.Header>
       <Modal.Body>
         <p>
+          Vi behöver spara och behandla personuppgifter om dig, så som för-och
+          efternamn, användarnamn och e-postadress. Syftet med en sådan
+          behandling är för att användaren ska kunna skapa ett individuellt
+          inloggningskonto och få en personlig användarupplevelse genom att ta
+          del av Booklys community och kunna recensera böcker.
+        </p>
+        <p>
+          Vi har fått dina uppgifter från dig när du registrerade ett
+          inloggningskonto hos Bookly. Tillhandahållande av personuppgifterna är
+          inte ett lagstadgat krav men observera att du behöver ange
+          personuppgifter för att kunna skapa ett inloggningskonto på Bookly. Vi
+          tillämpar vid var tid gällande integritetslagstiftning vid all
+          behandling av personuppgifter. Den rättsliga grunden för att behandla
+          dina personuppgifter är samtycke. Du har när som helst rätt att
+          återkalla ditt samtycke till behandlingen. Ett återkallande påverkar
+          inte lagligheten av behandlingen innan samtycket återkallades. Du kan
+          återkalla ditt samtycke genom att radera ditt konto via
+          Inställningar-fliken. Dina uppgifter kommer att sparas så länge du har
+          ett konto hos oss och kommer raderas när du tar bort ditt konto.
+        </p>
+
+        <p>
           De personuppgifter vi behandlar om dig delas med Bookly. Vi kan även
           komma att dela dina personuppgifter med en tredje part, förutsatt att
           vi är skyldiga att göra så enligt lag. Däremot kommer vi aldrig att
-          överföra dina uppgifter till ett land utanför EU.{" "}
+          överföra dina uppgifter till ett land utanför EU.
         </p>
 
-        <h2 style={{ fontSize: "20px" }}>
+        <h2 style={{ fontSize: '20px' }}>
           Personuppgiftsansvariga/dataskyddsombud är:
         </h2>
 
@@ -42,16 +64,16 @@ export default function DataProtectionPolicy({ showModal, onClose }) {
           uppgifter vi har om dig, för att begära rättelse, överföring eller för
           att begära att vi begränsar behandlingen, för att göra invändningar
           eller begära radering av dina uppgifter. Detta gör du enklast genom
-          att kontakta oss på{" "}
-          <span className="text-primary">anton.karlsson@iths.se</span>,{" "}
-          <span className="text-primary">frida.wikman@iths.se</span> och{" "}
+          att kontakta oss på{' '}
+          <span className="text-primary">anton.karlsson@iths.se</span>,{' '}
+          <span className="text-primary">frida.wikman@iths.se</span> och{' '}
           <span className="text-primary">maja.lennevi@iths.se</span>.
         </p>
 
         <p>
           Om du har klagomål på vår behandling av dina personuppgifter har du
           rätt att inge klagomål till tillsynsmyndigheten
-          Integritetsskydsmyndigheten, IMY.{" "}
+          Integritetsskydsmyndigheten, IMY.{' '}
         </p>
       </Modal.Body>
       <Modal.Footer>
@@ -60,5 +82,5 @@ export default function DataProtectionPolicy({ showModal, onClose }) {
         </Button>
       </Modal.Footer>
     </Modal>
-  );
+  )
 }
