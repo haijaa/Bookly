a67c8a8b27515b1cd20f8737d3bb4599a5611a13
haijaa <antkarl11@gmail.com>
2024-11-12 14:07:20 +0100

    Small tweaks styling

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,19 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../index.css";
 
 export default function Footer() {
   return (
     <footer>
       <p
-        className="text-center m-0 pt-5 white-text"
+        className="text-center m-0 pb-3 pt-5 white-text"
         style={{
           fontSize: 12,
-          backgroundColor: '#606D5D',
+          backgroundColor: "#606D5D",
         }}
       >
         Skolprojekt av JSU23 It-Högskolan. Information och bilder hämtade från
         akademibokhandeln.se.
       </p>
     </footer>
-  )
+  );
 }
